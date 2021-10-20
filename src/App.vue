5743b442478aca0d4c540b2ec70638d4973da89c
Forgotosleep <markbrian33@gmail.com>
2021-10-20 10:54:20 +0700

    Finished login & register feature

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link> |
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+  /* body {
+    background-color: black;
+  } */
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
